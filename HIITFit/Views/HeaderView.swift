1381e875960863f4d82a6145f72b436d83946252
alirezatab <alirezatabrizi10@gmail.com>
2024-11-28 23:23:31 -0800

    added a different way of syling the pagination page numbers based on my version vs ray wanderlich version

--- HIITFit/Views/HeaderView.swift
+++ HIITFit/Views/HeaderView.swift
@@ -46,7 +46,18 @@ struct HeaderView: View {
         .foregroundStyle(.white)
       HStack {
         ForEach(Exercise.exercises.indices, id: \.self) { index in
+          // the challenge / ray wanderlich way of doing it
+          ZStack {
+            Circle()
+              .frame(width: 32, height: 32)
+              .foregroundStyle(.white)
+              .opacity(index == selectedTab ? 0.5 : 0.0)
+            Circle()
+              .frame(width: 16, height: 16)
+              .foregroundStyle(.white)
+          }
           // My attempt
+          /*
           ZStack {
             Image(systemName: "circle.fill")
               .resizedToFill(width: 32, height: 32)
@@ -57,9 +68,11 @@ struct HeaderView: View {
               .resizedToFill(width: 16, height: 16)
               .foregroundStyle(.white)
           }
+           */
           .onTapGesture {
             selectedTab = index
           }
+          
         }
       }
       .font(.title2)
